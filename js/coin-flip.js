dfbe2e33ee926a1342af6a4e00a9947c07920c67
micah-rose <micah.r.lund@gmail.com>
2018-12-05 15:16:20 -0700

    final javascript version of CSIS 1410 java coin flip program

--- js/coin-flip.js
+++ js/coin-flip.js
@@ -1,27 +1,69 @@
-//TO DO's:
-//add functionality to flip and reset buttons
-// display flips in heads/tails textboxes
-//set multiple flips checkbox to flip whatever number is in textbox
-//change image depending on flip
-//random number generator for flips
+
 var heads = 0;
 var tails = 0;
-var rand = Math.floor(Math.random()*2) + 1; 
-var checkbox = document.getElementById("chkbx");
+var rand;
+var checkbox;
 
-function flipCoin(){ //working as expected
-console.log("Coin is flipped");
+//Functionality for 'Flip' button - working as expected
+function flipCoin(){ 
+    var numToss = document.getElementById("multi-flip").value;
+    checkbox = document.getElementById("chkbx");
+    rand = Math.ceil(Math.random()*2);
+
+    if(checkbox.checked){
+        for (i = 0; i < numToss; i++){
+            rand = Math.ceil(Math.random()*2);
+
+            if(rand == 1){
+                heads++;
+            }
+            else if(rand == 2){
+                tails++;
+            }
+        }
+
+        document.getElementById("heads-box").value = heads;
+        document.getElementById("tails-box").value = tails;
+
+        heads = 0;
+        tails = 0;
+    }else {
+        if(rand == 1){
+            heads++;
+            document.getElementById("coin-image").src = "/img/heads.png";
+            document.getElementById("heads-box").value = heads;
+        }
+        else if(rand == 2){
+            tails++;
+            document.getElementById("coin-image").src = "/img/tails.png";
+            document.getElementById("tails-box").value = tails;
+        }
+    }
 } 
 
-function resetFlips(){ //working as expected
-    console.log("Flips are reset");
-
+//Functionality for 'Reset' button
+function resetFlips(){ 
+    document.getElementById("multi-flip").readOnly = true;
+    document.getElementById("coin-image").src = "/img/question.png";
+    document.getElementById("heads-box").value = "";
+    document.getElementById("tails-box").value = "";
+    document.getElementById("chkbx").checked = false;
+    document.getElementById("multi-flip").value = "10";
+    document.getElementById("multi-flip").readOnly = true;
+    heads = 0;
+    tails = 0;
 }
 
-function multiFlips(){ //not working as expected - FIX ME
-    if(checkbox.checked === true){
+//Functionality for when 'Run Multiple Flips' box is checked - working as expected
+function multiFlips(){ 
+    checkbox = document.getElementById("chkbx");
+    if(checkbox.checked){
+        document.getElementById("multi-flip").readOnly = false;
+        document.getElementById("coin-image").src = "/img/heads-tails.png";
         console.log("Checked!!");
     } else {
+        document.getElementById("multi-flip").readOnly = true;
+        document.getElementById("coin-image").src = "/img/question.png";
         console.log("Not checked!!");
     }
 }
